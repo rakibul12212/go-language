09d56e24b56067f8dac0307a6fad2357892b67db
rakib12212 <rakibulislamr83@gmail.com>
2025-03-17 02:21:09 +0600

    how to print

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main()  {
+	
+fmt.Println("rakib")
+fmt.Println("md rakibul islam 26 years old")
+fmt.Println("from Dhaka,Bangladesh")
+fmt.Println("i am Front end developer")
+}
