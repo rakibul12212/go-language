52f830ab81b896b600c4506dc41855217eb0617d
rakib12212 <rakibulislamr83@gmail.com>
2025-03-17 02:34:15 +0600

    use of scape sequesce

--- main.go
+++ main.go
@@ -4,8 +4,8 @@ import "fmt"
 
 func main()  {
 	
-fmt.Println("rakib")
-fmt.Println("md rakibul islam 26 years old")
+fmt.Println("md rakibul islam \n he is 26 years old")
+
 fmt.Println("from Dhaka,Bangladesh")
 fmt.Println("i am Front end developer")
 }
---
+++ scape-sequence.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("1 \n2") //scape seq for new line
+	fmt.Println("name \t age")
+	fmt.Println("rakib \t 26")
+	fmt.Println("rakib \\ 26")
+	fmt.Println("\"rakib  26\"")
+}
